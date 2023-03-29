712305c7adecd291fb9a12f64598da332fdf87bc
DEIVE OLIVEIRA DOS ANJOS <deive.oanjos@senacsp.edu.br>
2023-03-29 19:32:08 -0300

    first commit

---
+++ src/main/java/br/com/carsolft/CreateCarServlet.java
@@ -0,0 +1,25 @@
+package br.com.carsolft;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/senac")
+
+public class CreateCarServlet  extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String comentario = request.getParameter("comentario");
+
+        System.out.println(comentario);
+
+
+
+        request.getRequestDispatcher("Sobre.html").forward(request, response);
+
+    }
+}
---
+++ target/carsoft/index.jsp
@@ -0,0 +1,15 @@
+<html>
+<body>
+<h1>Hello World!</h1>
+<a href="Sobre.html">sobre</a>
+
+<form method="post" action="/senac">
+
+<input type="text" name="comentario" id="comentario">
+
+<button id="submit" type="submit">enviar</button>
+
+
+</form>
+</body>
+</html>
